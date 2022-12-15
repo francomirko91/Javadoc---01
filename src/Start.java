c9e8a581929960d2aabc3594d47f645c14aa090b
Mirko Franco <francomirko91@gmail.com>
2022-12-15 10:23:18 +0100

    -----------------

---
+++ src/Start.java
@@ -0,0 +1,18 @@
+public class Start {
+
+    /**
+     * @param args
+     * @author Mirko Franco
+     */
+    public static void main(String[] args) {
+        WordGames game = new WordGames();
+
+
+        game.addHelloWord("Word");
+
+
+        game.getFullName("Mirko", "Franco");
+
+
+    }
+}
---
+++ src/WordGames.java
@@ -0,0 +1,22 @@
+public class WordGames {
+
+
+    /**
+     * this method create the string "Hello Word"
+     * @param word this parameter add "word" to the string
+     */
+    void addHelloWord(String word){
+         System.out.println("Hello " + word);
+     }
+
+    /**
+     * this method create the string "Hello Word"
+     * @param name this parameter add the name to the string
+     * @param surname this parameter add the surname to the string
+     */
+    void getFullName(String name,String surname){
+        System.out.println(name +" "+ surname);
+    }
+
+
+}
